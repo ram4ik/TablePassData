9953845f835b86fb547efa101c10cc103b9d98a1
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-10-20 09:23:09 +0300

    static false bool

--- TablePassData/BankPoint.swift
+++ TablePassData/BankPoint.swift
@@ -12,8 +12,8 @@ struct BankPoint: Codable {
     let t: Int
     let n, a: String
     let av, r: String?
-    let ncash: Bool
+    static let ncash: Bool = false
     let lat, lon: Double
     let i: String?
-    let cs: Bool
+    static let cs: Bool = false
 }
